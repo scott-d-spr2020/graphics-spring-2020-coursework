18f40e8ecf68f73f7cb7162e8c5beb46598b6611
Cameron Schneider <cameron.schneider@mymail.champlain.edu>
2020-01-24 18:52:29 -0500

    Adding cell shader

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/02-shading/drawNonphoto_multi_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/02-shading/drawNonphoto_multi_fs4x.glsl
@@ -31,10 +31,57 @@
 //	4) implement nonphotorealistic shading model
 //	Note: test all data and inbound values before using them!
 
+const int maxLightCount = 4;
+
+in PassData
+{
+	vec4 outNormal;
+	vec2 outTexCoord;
+	vec4 outViewPosition;
+} inData;
+
 out vec4 rtFragColor;
+
+uniform LightingUniforms
+{
+	int uLightCt;
+	int uLightSz;
+	int uLightSzInvSq;
+	vec4 uLightPos[maxLightCount];	//In render.c, lines 456-460, the lighting uniforms are sent. Lines 459 and 460 actually send arrays of uLightPos and uLightCol witht the "->v"
+	vec4 uLightCol[maxLightCount];
+} lightData;
+
+//General uniforms (GLSL forces sampler2D to be outside of blocks).
+uniform sampler1D rampTex;
+uniform vec4 uColor;
+
+float CalculateDiffuseCoefficient(vec4 norm, int index)
+{
+	//Just like with the normal vector, the light vector needs to be normalized for a proper dot product
+	vec4 L_vector = normalize(lightData.uLightPos[index]- inData.outViewPosition);
+
+	//Like in the book, we need to take the dot product which is the intensity. Color cannot be negative, hence the max.
+	float dotProd = max(0.0f, dot(norm, L_vector));
+
+	/*
+	For regular diffuse lighting
+	//Multiply the light color by the dot product scalar to get the color
+	//vec4 diffuseResult = lightData.uLightCol[index] * dotProd;
+	//return diffuseResult;
+	*/
+	return dotProd;
+}
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE BLUE
-	rtFragColor = vec4(0.0, 0.0, 1.0, 1.0);
+	//normalize normal vector to account for scale
+	vec4 outNormal_normalized = normalize(inData.outNormal);
+
+	float diffuseCoeff = 0.0;
+	for(int i = 0; i < lightData.uLightCt; i++)
+	{
+		diffuseCoeff += CalculateDiffuseCoefficient(outNormal_normalized, i);
+	}
+
+	rtFragColor = texture(rampTex, diffuseCoeff) * (diffuseCoeff * 0.8 + 0.2);
 }
