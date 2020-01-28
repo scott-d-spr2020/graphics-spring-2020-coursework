6a0a7f4f4d4acea525931f4112b291c0bdeefed5
Scott Dagen <scott.dagen@mymail.champlain.edu>
2020-01-28 10:37:18 -0500

    implemented passTexCoord

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/vs/02-shading/passTexcoord_transform_vs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/vs/02-shading/passTexcoord_transform_vs4x.glsl
@@ -34,9 +34,15 @@
 //	6) correctly transform input texture coordinate by atlas matrix
 
 layout (location = 0) in vec4 aPosition;
+layout (location = 8) in vec4 aTexCoord;
+uniform mat4 uMVP;
+uniform mat4 uAtlas;
+
+out vec2 vTexCoord;
 
 void main()
 {
 	// DUMMY OUTPUT: directly assign input position to output position
-	gl_Position = aPosition;
+	gl_Position = uMVP * aPosition;
+	vTexCoord = vec2(uAtlas * aTexCoord);
 }
