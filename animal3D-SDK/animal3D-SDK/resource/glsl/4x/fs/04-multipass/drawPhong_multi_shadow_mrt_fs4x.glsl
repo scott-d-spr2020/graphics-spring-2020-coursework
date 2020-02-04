e2f7b36df8ad55e4cc1adea4ebb84e753d185979
Cameron Schneider <cameron.schneider@mymail.champlain.edu>
2020-02-04 10:47:45 -0500

    Close to shadow mapping!

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/04-multipass/drawPhong_multi_shadow_mrt_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/04-multipass/drawPhong_multi_shadow_mrt_fs4x.glsl
@@ -32,10 +32,96 @@
 //	3) declare shadow map texture
 //	4) perform shadow test
 
-out vec4 rtFragColor;
+//code taken from Lab 2
+const int maxLightCount = 4;
+
+in CoordData
+{
+	vec2 texCoord;
+	vec4 mvPosition;
+	vec4 mvNormal;
+	vec4 shadowCoord;
+} coordData;
+
+uniform sampler2D mainTex;
+uniform sampler2D uTex_sm;
+uniform int uLightCt;
+uniform int uLightSz;
+uniform int uLightSzInvSq;
+uniform vec4 uLightPos[maxLightCount];
+uniform vec4 uLightCol[maxLightCount];
+uniform vec4 uColor;
+
+uniform mat4 uMV;
+
+layout (location = 0) out vec4 rtFragColor;
+layout (location = 1) out vec4 rtViewPosition;
+layout (location = 2) out vec4 rtNormal;
+layout (location = 3) out vec4 rtTexCoord;
+layout (location = 4) out vec4 rtDiffuseMap;
+layout (location = 5) out vec4 rtSpecularMap;
+layout (location = 6) out vec4 rtDiffuseTotal;
+layout (location = 7) out vec4 rtSpecularTotal;
+
+const int power = 16;
+const vec3 ambientColor = vec3(0.1f);
+
+struct LambertData
+{
+	vec4 LVec;
+	float dotProd_LN;
+};
+
+vec4 CalculateDiffuse(vec4 NVec, int index, out LambertData lambert)
+{
+	vec4 LVec = normalize(uLightPos[index]- coordData.mvPosition); //w coord is zero, probably
+	float dotProd_LN = dot(NVec, LVec);
+	lambert = LambertData(LVec, dotProd_LN);
+	float dotProd = max(0.0f, dotProd_LN);
+
+	vec4 diffuseResult = uLightCol[index] * dotProd;
+
+	return diffuseResult;
+}
+
+vec4 CalculateSpecular(vec4 NVec, int index, LambertData lambert, vec3 VVec3d, out float specValue)
+{
+	vec3 NVec3d = NVec.xyz;
+	vec3 LVec3d = lambert.LVec.xyz; //unsure if this is actually necessary
+	vec3 RVec3d = (2.0f * lambert.dotProd_LN * NVec3d) - LVec3d;
+	specValue = pow(max(0.0f, dot(VVec3d, RVec3d)), power);
+	return specValue * uLightCol[index];
+}
+
 
 void main()
 {
-	// DUMMY OUTPUT: all fragments are OPAQUE GREEN
-	rtFragColor = vec4(0.0, 1.0, 0.0, 1.0);
+	//this part's the same as Lambert
+	vec4 mvNormal_normalized = normalize(coordData.mvNormal);
+
+	vec4 diffuse = vec4(0.0, 0.0, 0.0, 1.0);
+	vec4 specular = vec4(0.0, 0.0, 0.0, 1.0);
+	float specVal = 0.0f;
+	vec3 VVec3d = normalize(-coordData.mvPosition.xyz);
+	for(int i = 0; i < uLightCt; i++)
+	{
+		LambertData lambert;
+		vec4 tempDiff = CalculateDiffuse(mvNormal_normalized, i, lambert);
+		vec4 tempSpec = CalculateSpecular(mvNormal_normalized, i, lambert, VVec3d, specVal);
+		specular += tempSpec;
+		diffuse += tempDiff;
+	}
+	vec4 diffColor = texture(mainTex, coordData.texCoord) * diffuse;
+	vec4 specularColor = texture(mainTex, coordData.texCoord) * specular;
+
+	vec4 perspDivideShadowCoord = coordData.shadowCoord / coordData.shadowCoord.w;
+
+	rtFragColor = vec4(diffColor.rgb + specularColor.rgb + (0.3f * ambientColor), 1.0);
+	rtViewPosition = coordData.mvPosition;
+	rtNormal =  vec4(mvNormal_normalized.xyz, 1.0);
+	rtTexCoord = vec4(coordData.texCoord, 0.0, 1.0);
+	rtDiffuseMap = texture(mainTex, coordData.texCoord);
+	rtSpecularMap = texture(uTex_sm, coordData.texCoord);
+	rtDiffuseTotal = diffuse;
+	rtSpecularTotal = specular;
 }
