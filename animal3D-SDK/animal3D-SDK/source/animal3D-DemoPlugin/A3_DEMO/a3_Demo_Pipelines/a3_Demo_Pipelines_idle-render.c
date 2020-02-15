b3d2fe1fdf679f29271355e78240db3271fb5f4a
Cameron Schneider <cameron.schneider@mymail.champlain.edu>
2020-02-15 12:59:17 -0500

    Adding vertical blur pass

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Demo_Pipelines/a3_Demo_Pipelines_idle-render.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Demo_Pipelines/a3_Demo_Pipelines_idle-render.c
@@ -558,27 +558,29 @@ void a3pipelines_render(a3_DemoState const* demoState, a3_Demo_Pipelines const* 
 	currentReadFBO = readFBO[currentPass][0];
 	a3framebufferActivate(currentWriteFBO);
 
-	a3framebufferBindColorTexture(currentReadFBO, a3tex_unit00 + 2, 0);
+	a3framebufferBindColorTexture(currentReadFBO, a3tex_unit00, 0);
 	
 	a3shaderUniformSendFloat(a3unif_vec2, currentDemoProgram->uAxis, 1, sampleAxisH.v);
 	
-
 	a3vertexDrawableRenderActive();
 
 	
 
 	// ****TO-DO: 
 	//	-> 3.1c: repeat previous pass but using vertical axis
-	sampleAxisV = a3vec2_y;	// delete this line; variable is already initialized
-	/*
+	//sampleAxisV = a3vec2_y;	// delete this line; variable is already initialized
+	
 	currentPass = pipelines_passBlurV_2;
 	currentWriteFBO = writeFBO[currentPass];
 	currentReadFBO = readFBO[currentPass][0];
-	???
-	???
-	???
-	???
-	*/
+	a3framebufferActivate(currentWriteFBO);
+
+	a3framebufferBindColorTexture(currentReadFBO, a3tex_unit00, 0);
+
+	a3shaderUniformSendFloat(a3unif_vec2, currentDemoProgram->uAxis, 1, sampleAxisV.v);
+
+	a3vertexDrawableRenderActive();
+	
 
 	// ****TO-DO: 
 	//	-> 4.1f: repeat bright pass and blur passes on smaller FBOs
