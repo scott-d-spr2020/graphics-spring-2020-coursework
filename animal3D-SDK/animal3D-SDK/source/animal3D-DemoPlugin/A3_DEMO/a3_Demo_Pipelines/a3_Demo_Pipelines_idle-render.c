2b161b02c9aec065728f0ca608830afb4af1861e
Scott Dagen <scott.dagen@mymail.champlain.edu>
2020-02-15 13:50:37 -0500

    Added more read buffers?

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Demo_Pipelines/a3_Demo_Pipelines_idle-render.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Demo_Pipelines/a3_Demo_Pipelines_idle-render.c
@@ -287,12 +287,15 @@ void a3pipelines_render(a3_DemoState const* demoState, a3_Demo_Pipelines const* 
 		
 		{ demoState->fbo_post_c16_2fr + 0, 0, },
 		{ demoState->fbo_post_c16_2fr + 1, 0, },
+		{ demoState->fbo_post_c16_2fr + 2, 0, },
 
 		{ demoState->fbo_post_c16_4fr + 0, 0, },
 		{ demoState->fbo_post_c16_4fr + 1, 0, },
+		{ demoState->fbo_post_c16_4fr + 2, 0, },
 
 		{ demoState->fbo_post_c16_8fr + 0, 0, },
 		{ demoState->fbo_post_c16_8fr + 1, 0, },
+		{ demoState->fbo_post_c16_8fr + 2, 0, },
 
 		
 		// ****TO-DO: 
