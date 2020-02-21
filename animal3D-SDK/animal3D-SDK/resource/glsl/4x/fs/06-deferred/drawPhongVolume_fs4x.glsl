4d16d94b9d9bd1a5d8d7936b8e8114f211445543
Cameron Schneider <cameron.schneider@mymail.champlain.edu>
2020-02-21 10:28:19 -0500

    Merging volume and deferred

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/06-deferred/drawPhongVolume_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/06-deferred/drawPhongVolume_fs4x.glsl
@@ -61,6 +61,60 @@ uniform ubPointLight
 layout (location = 6) out vec4 rtDiffuseLight;
 layout (location = 7) out vec4 rtSpecularLight;
 
+
+struct LambertData
+{
+	vec4 LVec;
+	float dotProd_LN;
+};
+
+
+vec4 CalculateDiffuse(vec4 NVec, int index, vec4 position, out LambertData lambert)
+{
+	vec4 LVec = normalize(uLightPos[index] - position); //w coord is zero, probably
+	float dotProd_LN = dot(NVec, LVec);
+	lambert = LambertData(LVec, dotProd_LN);
+	float dotProd = max(0.0f, dotProd_LN);
+
+	vec4 diffuseResult = uLightCol[index] * dotProd;
+
+	return diffuseResult;
+}
+
+
+//calculates specular highlight.
+vec4 CalculateSpecular(vec4 NVec, int index, LambertData lambert, vec3 VVec3d)
+{
+	vec3 NVec3d = NVec.xyz;
+	vec3 LVec3d = lambert.LVec.xyz; //unsure if this is actually necessary
+	vec3 RVec3d = (2.0f * lambert.dotProd_LN * NVec3d) - LVec3d;
+
+	//pow is 16
+	float tempSpecVal = max(0.0f, dot(VVec3d, RVec3d));
+	float powVal = tempSpecVal * tempSpecVal; //^2
+	powVal = powVal * powVal; //^4
+	powVal = powVal * powVal; //^8
+	powVal = powVal * powVal; //^16
+	return powVal * uLightCol[index];
+}
+
+
+vec3 CalculatePosition()
+{
+	vec3 sampledPos = texture(uImage01, vTexcoord.xy).rgb; //gives us position previously saved
+	//that data's [0,1], when we need [-x,x]
+	vec4 sampledDepth = texture(uImage00, vTexcoord.xy);
+
+	vec4 recalculatedPos = vec4(sampledPos.x, sampledPos.y, sampledDepth.z, 1.0);
+	//recalculatedPos.z = 2.0 * recalculatedPos.z - 1.0;	//reset depth value to [-1, 1]
+	recalculatedPos = uPB_inv * recalculatedPos;
+
+	return (recalculatedPos / recalculatedPos.w).xyz;
+}
+
+
+
+
 void main()
 {
 	// DUMMY OUTPUT: all fragments are OPAQUE MAGENTA
