06107259edbee7f1c1963daae5b43c78cfb574e1
Scott Dagen <scott.dagen@mymail.champlain.edu>
2020-02-24 12:19:09 -0500

    Why does this not-work in a different way than I expected

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/06-deferred/drawPhongVolume_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/06-deferred/drawPhongVolume_fs4x.glsl
@@ -103,6 +103,20 @@ vec4 CalculateSpecular(vec4 NVec, LambertData lambert, vec3 VVec3d)
 	return powVal * lights[vInstanceID].color;
 }
 
+/*
+vec3 CalculatePosition()
+{
+	vec3 sampledPos = texture(uImage01, vTexcoord.xy).rgb; //gives us position previously saved
+	//that data's [0,1], when we need [-x,x]
+	vec4 sampledDepth = texture(uImage00, vTexcoord.xy);
+
+	vec4 recalculatedPos = vec4(sampledPos.x, sampledPos.y, sampledDepth.z, 1.0);
+	//recalculatedPos.z = 2.0 * recalculatedPos.z - 1.0;	//reset depth value to [-1, 1]
+	recalculatedPos = uPB_inv * recalculatedPos;
+
+	return (recalculatedPos / recalculatedPos.w).xyz;
+}*/
+
 
 vec3 CalculatePosition(out vec4 sampleCoord)
 {
@@ -126,6 +140,7 @@ vec3 CalculatePosition(out vec4 sampleCoord)
 
 void main()
 {
+	normalize(vBiasedClipCoord/vBiasedClipCoord.w);
 	vec4 sampleCoord;
 
 	vec3 position = CalculatePosition(sampleCoord);
@@ -143,6 +158,7 @@ void main()
 
 	//rtViewPosition = vec4(position, 1.0f);
 	//rtViewPosition = vBiasedClipCoord;
+	rtViewPosition = normalize(vBiasedClipCoord/vBiasedClipCoord.w);
 	rtDiffuseLight = diffuse;
 	rtSpecularLight = specular;
 }
