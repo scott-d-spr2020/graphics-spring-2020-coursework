56a371e651712b0ff6b439e91472afbc1c39b628
Cameron Schneider <cameron.schneider@mymail.champlain.edu>
2020-02-29 13:46:55 -0500

    Adding in new shader files, programs

---
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/midterm-SSAO-Cross/drawPhong_cross_deferred_fs4x.glsl
@@ -0,0 +1,160 @@
+/*
+	Copyright 2011-2020 Daniel S. Buckstein
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+/*
+	animal3D SDK: Minimal 3D Animation Framework
+	By Daniel S. Buckstein
+	
+	drawPhong_multi_deferred_fs4x.glsl
+	Draw Phong shading model by sampling from input textures instead of 
+		data received from vertex shader.
+*/
+
+#version 410
+
+#define MAX_LIGHTS 4
+
+// ****TO-DO: 
+//	0) copy original forward Phong shader
+//	1) declare g-buffer textures as uniform samplers
+//	2) declare light data as uniform block
+//	3) replace geometric information normally received from fragment shader 
+//		with samples from respective g-buffer textures; use to compute lighting
+//			-> position calculated using reverse perspective divide; requires 
+//				inverse projection-bias matrix and the depth map
+//			-> normal calculated by expanding range of normal sample
+//			-> surface texture coordinate is used as-is once sampled
+
+in vec4 vTexcoord;
+
+const vec3 ambientColor = vec3(0.1f);
+
+uniform sampler2D uImage00; // g-buffer depth texture
+uniform sampler2D uImage01; // g-buffer position texture
+uniform sampler2D uImage02; // g-buffer normal texture
+uniform sampler2D uImage03; // g-buffer texcoord texture
+							   
+uniform sampler2D uImage04; // diffuse texture atlas
+uniform sampler2D uImage05; // spec texture atlas
+
+
+uniform int uLightCt;
+uniform int uLightSz;
+uniform int uLightSzInvSq;
+uniform vec4 uLightPos[MAX_LIGHTS];
+uniform vec4 uLightCol[MAX_LIGHTS];
+uniform vec4 uColor;
+
+
+uniform mat4 uPB_inv;
+
+
+struct LambertData
+{
+	vec4 LVec;
+	float dotProd_LN;
+};
+
+
+layout (location = 0) out vec4 rtFragColor;
+layout (location = 1) out vec4 rtPosition;
+layout (location = 2) out vec4 rtNormal;
+layout (location = 3) out vec4 rtTexcoord;
+layout (location = 4) out vec4 rtDiffuseMapSample;
+layout (location = 5) out vec4 rtSpecularMapSample;
+layout (location = 6) out vec4 rtDiffuseLightTotal;
+layout (location = 7) out vec4 rtSpecularLightTotal;
+
+
+vec4 CalculateDiffuse(vec4 NVec, int index, vec4 position, out LambertData lambert)
+{
+	vec4 LVec = normalize(uLightPos[index] - position); //w coord is zero, probably
+	float dotProd_LN = dot(NVec, LVec);
+	lambert = LambertData(LVec, dotProd_LN);
+	float dotProd = max(0.0f, dotProd_LN);
+
+	vec4 diffuseResult = uLightCol[index] * dotProd;
+
+	return diffuseResult;
+}
+
+
+//calculates specular highlight.
+vec4 CalculateSpecular(vec4 NVec, int index, LambertData lambert, vec3 VVec3d)
+{
+	vec3 NVec3d = NVec.xyz;
+	vec3 LVec3d = lambert.LVec.xyz; //unsure if this is actually necessary
+	vec3 RVec3d = (2.0f * lambert.dotProd_LN * NVec3d) - LVec3d;
+
+	//pow is 16
+	float tempSpecVal = max(0.0f, dot(VVec3d, RVec3d));
+	float powVal = tempSpecVal * tempSpecVal; //^2
+	powVal = powVal * powVal; //^4
+	powVal = powVal * powVal; //^8
+	powVal = powVal * powVal; //^16
+	return powVal * uLightCol[index];
+}
+
+
+vec3 CalculatePosition()
+{
+	vec3 sampledPos = texture(uImage01, vTexcoord.xy).rgb; //gives us position previously saved
+	//that data's [0,1], when we need [-x,x]
+	vec4 sampledDepth = texture(uImage00, vTexcoord.xy);
+
+	vec4 recalculatedPos = vec4(sampledPos.x, sampledPos.y, sampledDepth.z, 1.0);
+	//recalculatedPos.z = 2.0 * recalculatedPos.z - 1.0;	//reset depth value to [-1, 1]
+	recalculatedPos = uPB_inv * recalculatedPos;
+
+	return (recalculatedPos / recalculatedPos.w).xyz;
+}
+
+
+void main()
+{
+	vec2 texCoord = texture(uImage03, vTexcoord.xy).rg; // Indidivual texture coords are stored in this texture's rg channels
+	vec3 position = CalculatePosition();
+	vec4 normal = vec4(texture(uImage02, vTexcoord.xy).xyz, 1.0) * 2.0f - vec4(1.0f); //uncompress
+
+	vec4 diffuse = vec4(0.0, 0.0, 0.0, 1.0);
+	vec4 specular = vec4(0.0, 0.0, 0.0, 1.0);
+	vec3 VVec3d = normalize(-position.xyz);
+
+	for(int i = 0; i < uLightCt; i++)
+	{
+		LambertData lambert;
+		vec4 tempDiff = CalculateDiffuse(normal, i, vec4(position, 1.0), lambert);
+		vec4 tempSpec = CalculateSpecular(normal, i, lambert, VVec3d);
+		specular += tempSpec;
+		diffuse += tempDiff;
+	}
+
+	vec4 diffuseSample = texture(uImage04, texCoord);
+	vec4 specularSample =  texture(uImage05, texCoord);
+
+	vec4 diffColor = diffuseSample * diffuse;
+	vec4 specularColor = specularSample * specular;
+
+	rtTexcoord = vec4(texCoord, 0.0, 1.0);
+	rtNormal = normal;
+	rtPosition = vec4(position, 1.0);
+
+	rtFragColor = vec4(diffColor.rgb + specularColor.rgb + (0.3f * ambientColor), 1.0);
+	rtDiffuseMapSample = vec4(diffuseSample.rgb, 1.0f);
+	rtSpecularMapSample = specularSample;
+	rtDiffuseLightTotal = diffuse;
+	rtSpecularLightTotal = specular;
+}
